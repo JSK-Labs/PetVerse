85226e314295b9d656dd528a8285f2b577617559
marvelousmicheal <donatusmarvelous09@gmail.com>
2025-06-30 23:50:37 +0100

    feat: create learn page and component structure

--- packages/nextjs/app/(petverse)/pages/learn.tsx
+++ packages/nextjs/app/(petverse)/pages/learn.tsx
@@ -107,7 +107,7 @@ export const LearnSection: React.FC = () => {
 
   return (
 
-    <section className='w-full h-full bg-white'>
+    <section className='w-full h-full  bg-gradient-to-br from-amber-50 to-blue-50'>
 
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       {/* Header */}
